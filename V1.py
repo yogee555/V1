dc01fb7ceba089cfbb94bfdbc7f2770d4fc2d167
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2024-06-25 11:12:06 +0530

    Add files via upload

---
+++ V1.py
@@ -0,0 +1,15 @@
+# Variables
+# find the execute of types & storage
+user_name = 'Viraj'                  # string
+age = 29                             # integer
+is_accepted_terms = True             # boolen
+account_balance = 3245.56            # float
+
+print(user_name)
+print(type(user_name))
+print(age)
+print(type(age))
+print(is_accepted_terms)
+print(type(is_accepted_terms))
+print(account_balance)
+print(type(account_balance))
